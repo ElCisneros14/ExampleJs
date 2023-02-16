78699acf1247d1be5900bc88f956fc26478c154d
ElCisneros14 <cisneroseliasdaniel@gmail.com>
2023-02-16 18:37:22 -0300

    example

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log('Hello Word!')
+
+let miNombre = "Elias"
+
+
+console.log(miNombre)
+
+let persona = {
+    nombre: 'Elias',
+    apellido: 'Cisneros',
+    edad: '37',
+    estadoCivil: 'Casado',
+    hijos: 'si',
+    nombreHijaMayor: 'Candela',
+    nombreHijaMenor: 'Delfina',
+    nombreMascota: 'rocco',
+}
+
+    console.log(persona)
