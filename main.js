531d168cf3a8815bf6d62c123525a67b27d049c9
ElCisneros14 <cisneroseliasdaniel@gmail.com>
2023-02-22 15:32:42 -0300

    Loops

--- main.js
+++ main.js
@@ -28,3 +28,11 @@ let persona = {
     }
 
     console.log(mascota)
+
+    let contador = 0
+
+    while(contador < 3) {
+        console.log(contador)
+        contador++
+    }
+    console.log(contador)
